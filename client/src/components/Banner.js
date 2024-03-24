15ba3033c982764c0fdc15bda96830caedffe7ab
sanaullah963 <allhera963@gmail.com>
2024-03-25 00:06:29 +0600

    banner responsive

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -3,16 +3,16 @@ import React from "react";
 import bannerImage from '@/image/illustration-working.svg'
 function Banner() {
   return (
-    <main className="flex flex-col md:flex-row mt-20">
-      <div className="w-1/2">
-        <h1 className="text-5xl font-bold leading-[70px]">The Original URL SHORTENER</h1>
-        <p className=" text-gray-400 pe-5 mt-10">
+    <main className="flex flex-col lg:flex-row mt-20">
+      <div className="lg:w-1/2">
+        <h1 className="text-2xl sm:text-5xl font-bold md:leading-[70px]">The Original URL SHORTENER</h1>
+        <p className=" text-gray-400 sm:pe-5 mt-10">
           The complete URL Shortener platform, Link Management, Link Analytics,
           Deep links, QR Codes Generator, and Link in Bio. Shorten, brand,
           manage, track, and share your links effortlessly.
         </p>
       </div>
-      <div className="mt-[-70px]">
+      <div className="mt-[40px] lg:mt-[-70px]">
         <Image src={bannerImage} alt="banner image"/>
       </div>
     </main>
